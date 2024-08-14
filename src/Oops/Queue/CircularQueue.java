f34ebbb00099488ae32a8b0b9ac0856942f8748e
hEzHil <88553060+hEzHil@users.noreply.github.com>
2024-08-14 13:12:08 +0530

    updted stack and queue

---
+++ src/Oops/Queue/CircularQueue.java
@@ -0,0 +1,65 @@
+package Oops.Queue;
+
+public class CircularQueue {
+    protected int[] data ;
+    private static final int DEFAULT_SIZE = 10;
+   protected int end =0;
+   protected int front = 0;
+   private int size = 0;
+
+    public CircularQueue() {
+        this(DEFAULT_SIZE);
+    }
+    public CircularQueue(int size) {
+        this.data = new int[size];
+    }
+
+    private boolean isFull() {
+        return size == data.length; // prt at last index
+    }
+
+    private boolean isEmpty() {
+        return size == 0; // prt at last index
+    }
+
+    public boolean insert(int val){
+        if(isFull()){
+            return false;
+        }
+        data[end++] = val;
+        end = end % data.length;
+        size++;
+        return true;
+    }
+
+    public int remove() throws Exception{
+        if(isEmpty()){
+            throw new Exception("Queue is Empty");
+        }
+
+        int removed = data[front++];
+        front = front % data.length;
+        size--;
+        return removed;
+    }
+    public int front() throws Exception{
+        if(isEmpty()){
+            throw new Exception("Queue is Empty");
+        }
+        return data[front];
+    }
+    public void display(){
+        if(isEmpty()){
+            System.out.println("Queue is Empty ");
+            return;
+        }
+
+        int i  = front;
+        do{
+            System.out.print(data[i] + " -> ");
+            i++;
+            i %= data.length;
+        }while(i != end);
+        System.out.println("End");
+    }
+}
--- src/Oops/Queue/Main.java
+++ src/Oops/Queue/Main.java
@@ -2,22 +2,36 @@ package Oops.Queue;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        CustomQueue queue = new CustomQueue(3);
-        // it Follows the first in first out principle
-
-        // add the value to queue one by one
-        queue.insert(10);
-        queue.insert(45);
-        queue.insert(56);
-        queue.insert(35);
-        // display the queue
-        queue.display();
+//        CustomQueue queue = new CustomQueue(3);
+//        // it Follows the first in first out principle
+//
+//        // add the value to queue one by one
+//        queue.insert(10);
+//        queue.insert(45);
+//        queue.insert(56);
+//        queue.insert(35);
+//        // display the queue
+//        queue.display();
 
         // remove the first element in the queue
-        queue.remove();
-        queue.display();
-        // print the front value in the queue
-        System.out.println(queue.front());
+//        queue.remove();
+//        queue.display();
+//        // print the front value in the queue
+//        System.out.println(queue.front());
 
+        // ------------CIRCULAR QUEUE--------//
+        CircularQueue circularQueue = new CircularQueue();
+        circularQueue.insert(20);
+        circularQueue.insert(24);
+        circularQueue.insert(34);
+        circularQueue.insert(131);
+        circularQueue.insert(12);
+        circularQueue.display();
+        System.out.println(circularQueue.remove());
+        circularQueue.insert(2);
+        circularQueue.display();
+        System.out.println(circularQueue.remove());
+        circularQueue.display();
     }
+
 }
